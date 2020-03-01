976535b90e246e7eb8a9dcd090bff539cf4e09a8
Josh Bailey <josh@vandervecken.com>
2020-03-01 15:05:00 +1300

    Fix comment/github ref.

--- dram4164test.ino
+++ dram4164test.ino
@@ -1,24 +1,12 @@
+// Copyright 2020 Josh Bailey (josh@vandervecken.com)q
 
-// Copyright 2020 Josh Bailey (josh@vandervecken.com)
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to deal in
- the Software without restriction, including without limitation the rights to us
-e, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of t
-he Software, and to permit persons to whom the Software is furnished to do so, s
-ubject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-// The above copyright notice and this permission notice shall be included in al
-l copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM
-PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS F
-OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR CO
-PYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
-AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-// https://github.com/johnzl-777/DRAMutils
+// Requires: https://github.com/anarkiwi/DRAMutils
 // Missing from DRAMUtils docs:
 // DI is pin 9
 // DO is pin 10
